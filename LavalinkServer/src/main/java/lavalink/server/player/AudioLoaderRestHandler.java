4fbe057437bc71ce6e0fcee97874ae19aad69671
カグチ <59391215+KagChi@users.noreply.github.com>
2021-08-25 14:08:21 +0700

    ok then

--- LavalinkServer/src/main/java/lavalink/server/player/AudioLoaderRestHandler.java
+++ LavalinkServer/src/main/java/lavalink/server/player/AudioLoaderRestHandler.java
@@ -74,7 +74,7 @@ public class AudioLoaderRestHandler {
                 .put("isSeekable", audioTrack.isSeekable())
                 .put("position", audioTrack.getPosition())
                 .put("sourceName", audioTrack.getSourceManager() == null ? null : audioTrack.getSourceManager().getSourceName())
-                .put("artworkUrl", trackInfo.artworkUrl.isEmpty() ? trackInfo.artworkUrl : null);
+                .put("artworkUrl", trackInfo.artworkUrl);
     }
 
     private JSONObject encodeLoadResult(LoadResult result) {
