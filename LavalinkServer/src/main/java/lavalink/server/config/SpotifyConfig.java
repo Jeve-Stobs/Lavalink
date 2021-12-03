a14c47b6ec85f53d73e7fdf9940adc8c225c9bc1
KChi <59391215+KagChi@users.noreply.github.com>
2021-12-03 10:30:57 +0700

    Update SpotifyConfig.java

--- LavalinkServer/src/main/java/lavalink/server/config/SpotifyConfig.java
+++ LavalinkServer/src/main/java/lavalink/server/config/SpotifyConfig.java
@@ -4,10 +4,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by napster on 05.03.18.
+ * Created by KagChi on 03.12.21.
  */
 @ConfigurationProperties(prefix = "lavalink.server.spotify")
 @Component
 public class SpotifyConfig {
-    public String spotiLavaUrl = null;
+    public String spotiLavaUrl = "";
 }
